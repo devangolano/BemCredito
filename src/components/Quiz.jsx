37be814494ee0908568a3e23e305d654d5b7e1a9
alberto <albertoronny237@gmail.com>
2024-09-09 08:16:38 +0100

    fix

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -14,7 +14,7 @@ const Quiz = () => {
       <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-start sm:text-start mb-4">
         Por que empréstimo online no #BPC?
       </h2>
-      <p className="text-center text-gray-600 mb-8 max-w-md">
+      <p className="text-start text-gray-600 mb-8 max-w-md">
         Te ajudamos a concretizar seus objetivos: quitar uma dívida, reformar sua casa, abrir seu negócio ou resolver uma emergência. Como?
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-5xl">
