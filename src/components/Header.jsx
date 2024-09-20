1675bc7f37c3d723e1f0a850909dd5b76f754244
alberto <albertoronny237@gmail.com>
2024-09-20 02:01:17 +0100

    Closed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import InputMask from 'react-input-mask';
 import axios from "axios";
-import { useNavigate } from 'react-router-dom'; // Importando o hook useNavigate
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  const navigate = useNavigate(); // Hook para navegar entre páginas
+  const navigate = useNavigate(); 
   const [step, setStep] = useState(1);
   const [amount, setAmount] = useState(5000);
   const [months, setMonths] = useState("");
@@ -15,15 +15,15 @@ const Header = () => {
     whatsapp: "",
   });
   const [showModal, setShowModal] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false); // Estado para controlar o envio
 
   const validateCPF = (cpf) => {
-    cpf = cpf.replace(/[^\d]+/g, ""); // Remove caracteres não numéricos
-    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false; // Verifica se tem 11 dígitos e se não é uma sequência repetida
+    cpf = cpf.replace(/[^\d]+/g, ""); 
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
 
     let soma = 0;
     let resto;
 
-    // Validação do primeiro dígito verificador
     for (let i = 1; i <= 9; i++) {
       soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
     }
@@ -31,7 +31,6 @@ const Header = () => {
     if (resto === 10 || resto === 11) resto = 0;
     if (resto !== parseInt(cpf.substring(9, 10))) return false;
 
-    // Validação do segundo dígito verificador
     soma = 0;
     for (let i = 1; i <= 10; i++) {
       soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
@@ -74,23 +73,29 @@ const Header = () => {
   const sendEmail = async (e) => {
     e.preventDefault();
 
-    // Verifica se já passou o tempo de 10 minutos desde o último envio
+    if (isSubmitting) return; // Impede múltiplos envios
+
+    setIsSubmitting(true); // Bloqueia novos cliques no botão de envio
+
     const lastSubmitTime = localStorage.getItem('lastSubmitTime');
     const now = Date.now();
 
     if (lastSubmitTime && now - lastSubmitTime < 10 * 60 * 1000) {
       const minutesLeft = Math.ceil((10 * 60 * 1000 - (now - lastSubmitTime)) / 60000);
       alert(`Você precisa esperar ${minutesLeft} minutos antes de reenviar o formulário.`);
+      setIsSubmitting(false); // Libera o botão de envio
       return;
     }
 
     if (!formData.name || !formData.cef || !formData.email || !formData.whatsapp || !months) {
       alert("Por favor, preencha todos os campos...");
+      setIsSubmitting(false); // Libera o botão de envio
       return;
     }
 
     if (!validateCPF(formData.cef)) {
       alert("Por favor, insira um CPF válido.");
+      setIsSubmitting(false); // Libera o botão de envio
       return;
     }
 
@@ -111,7 +116,6 @@ const Header = () => {
         message: htmlTemplate,
       });
 
-      // Reseta os dados do formulário
       setFormData({
         name: '',
         cef: '',
@@ -123,15 +127,16 @@ const Header = () => {
 
       setShowModal(true);
 
-      // Salva o timestamp do envio no localStorage
       localStorage.setItem('lastSubmitTime', Date.now());
 
       setTimeout(() => {
-        setShowModal(false); // Fechar o modal
-        navigate('/'); // Redirecionar para a página inicial
-      }, 3000); // Tempo de exibição do modal antes de redirecionar
+        setShowModal(false);
+        window.location.href = "https://www.bompracredito.com.br/emprestimo-pessoal";
+      }, 1000);
     } catch (error) {
       console.error("Erro ao enviar o email:", error);
+    } finally {
+      setIsSubmitting(false); // Libera o botão de envio
     }
   };
 
@@ -145,7 +150,6 @@ const Header = () => {
           Simule seu empréstimo agora e saia do sufoco!
         </p>
 
-        {/* Passo 1: Selecionar o valor do empréstimo */}
         {step === 1 && (
           <div className="bg-white bg-opacity-90 border-t-4 border-green-500 p-6 md:h-96 mx-auto">
             <h2 className="text-lg font-semibold text-gray-800 md:mb-4">
@@ -181,7 +185,6 @@ const Header = () => {
           </div>
         )}
 
-        {/* Passo 2: Selecionar a quantidade de meses */}
         {step === 2 && (
           <div className="bg-white bg-opacity-90 border-t-4 border-green-500 p-6 md:h-96 mx-auto">
             <h2 className="text-lg font-semibold text-gray-800 md:mb-4">
@@ -219,73 +222,77 @@ const Header = () => {
           </div>
         )}
 
-        {/* Passo 3: Formulário de Dados Pessoais */}
         {step === 3 && (
-          <form className="bg-white bg-opacity-90 border-t-4 border-green-500 p-6 md:h-96 mx-auto">
+          <div className="bg-white bg-opacity-90 border-t-4 border-green-500 p-6 mx-auto">
             <h2 className="text-lg font-semibold text-gray-800 md:mb-4">
               Preencha seus dados:
             </h2>
-            <input
-              type="text"
-              name="name"
-              placeholder="Nome Completo"
-              className="border p-2 rounded-md w-full mb-4"
-              value={formData.name}
-              onChange={handleInputChange}
-            />
-            <InputMask
-              mask="999.999.999-99"
-              type="text"
-              name="cef"
-              placeholder="CPF"
-              className="border p-2 rounded-md w-full mb-4"
-              value={formData.cef}
-              onChange={handleInputChange}
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              className="border p-2 rounded-md w-full mb-4"
-              value={formData.email}
-              onChange={handleInputChange}
-            />
-            <InputMask
-              mask="(99) 99999-9999"
-              type="text"
-              name="whatsapp"
-              placeholder="Whatsapp"
-              className="border p-2 rounded-md w-full mb-4"
-              value={formData.whatsapp}
-              onChange={handleInputChange}
-            />
-            <div className="flex justify-between">
+            <form onSubmit={sendEmail}>
+            <div className="mb-4">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Nome Completo"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  className="border p-2 rounded-md w-full"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <InputMask
+                  mask="999.999.999-99"
+                  value={formData.cef}
+                  onChange={handleInputChange}
+                  name="cef"
+                  placeholder="CPF"
+                  className="border p-2 rounded-md w-full"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  className="border p-2 rounded-md w-full"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <InputMask
+                  mask="(99) 99999-9999"
+                  value={formData.whatsapp}
+                  onChange={handleInputChange}
+                  name="whatsapp"
+                  placeholder="WhatsApp"
+                  className="border p-2 rounded-md w-full"
+                  required
+                />
+              </div>
               <button
-                className="mt-4 bg-red-200 font-mono text-gray-600 font-bold py-2 px-4 rounded-full hover:bg-gray-500 transition duration-200"
-                onClick={handleBack}
+                type="submit"
+                className={`w-full bg-green-500 text-white py-2 px-4 rounded-md font-bold ${isSubmitting ? 'opacity-50 cursor-not-allowed' : 'hover:bg-green-600'}`}
+                disabled={isSubmitting} 
               >
-                ← Voltar
+                {isSubmitting ? 'Enviando...' : 'Enviar'}
               </button>
-              <button
-                className="mt-4 bg-green-500 font-mono text-gray-600 font-bold py-2 px-4 rounded-full hover:bg-green-600 transition duration-200"
-                onClick={sendEmail}
-              >
-                Enviar
-              </button>
+            </form>
+          </div>
+        )}
+        {showModal && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-6 rounded-lg">
+              <h2 className="text-lg font-semibold mb-4">Ficha enviada com sucesso!</h2>
+              <p className="text-sm">Redirecionando para a página de empréstimo...</p>
             </div>
-          </form>
+          </div>
         )}
       </div>
-
-      {showModal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg">
-            <h2 className="text-lg font-semibold mb-4">Ficha enviada com sucesso!</h2>
-            <p className="text-sm">Redirecionando para a página inicial...</p>
-          </div>
-        </div>
-      )}
     </div>
+    
   );
 };
 
