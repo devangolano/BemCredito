353a7dccd09d1d2dad141b7627b5b514e45406e0
Dev Angolano <albertoronny237@gmail.com>
2024-12-17 09:27:30 +0100

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -122,7 +122,7 @@ const Header = () => {
     `;
 
     try {
-      await axios.post("https://meuback-mmfq.onrender.com", {
+      await axios.post("https://meuback-mmfq.onrender.com/api/send", {
         from: "bempracredito@gmail.com",
         to: "fichasmarcuscarioca@gmail.com",
         subject: "Nova Ficha | Bem Pra Crédito",
